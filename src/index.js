e41e6896090654bb5c70e5910f02de06dde670de
Daraédna Menezes <daraedna@gmail.com>
2021-07-13 19:07:33 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component{
+  state = {
+    texto: "Turma React"
+  }
+  handleClick() {
+    alert("Alerta ativado")
+  }
+  handleTextChange(e) {
+    this.setState({
+      texto: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World, {this.props.name}</h1>
+        <h2>{this.state.texto}</h2>
+        <input type="text" onChange={this.handleTextChange.bind(this)} />
+        <button onClick={this.handleClick.bind(this)}>Exibir alerta</button>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name={"Daraédna Menezes"} idade={20}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
